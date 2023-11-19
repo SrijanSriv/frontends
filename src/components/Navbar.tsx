ef2d4fce12d2f2e5cc16f4004a1f22946c80436d
SrijanSriv <srijansrivastav01@gmail.com>
2023-11-19 20:06:22 +0530

    fix: navbar 😶

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,6 @@ export default function Navbar() {
                 <div className="flex">
                 <Link href={'/about'} className="bg-yellow-400 border-l-2 border-black pl-5 pr-5 pt-7 text-xl hover:bg-yellow-500">ABOUT</Link>
                 <Link href={'/library'} className="bg-pink-300 border-l-2 border-r-2 border-black pl-5 pr-5 pt-7 text-xl hover:bg-pink-400">LIBRARY</Link>
-                <Link href={'/dashboard'} className="bg-emerald-400 border-r-2 border-black pl-5 pr-5 pt-7 text-xl hover:bg-emerald-500">DBOARD</Link>
                 {user.isSignedIn?
                   <SignOutButton><button className="w-32 border-r-2 border-black bg-red-500 pl-5 pr-5 pt-1 text-xl w-30 hover:bg-red-600">SIGN OUT</button></SignOutButton> :
                   <SignInButton><button className="w-32 border-l-2 border-r-2 border-black bg-green-500 pl-5 pr-5 pt-1 text-xl w-30 hover:bg-green-600">SIGN IN</button></SignInButton>
